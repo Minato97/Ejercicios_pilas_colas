dbc869097e6ce2976b85eb3167c31def287db0c9
Minato 97 <ignacio.andrade8033@alumnos.udg.mx>
2022-05-05 15:03:38 -0500

    ya mero

--- Ejercicio_lista_enlazada 2.py
+++ Ejercicio_lista_enlazada 2.py
@@ -1,5 +1,6 @@
 import time
 
+i = 0
 class _Nodo:
     __slots__='_elemento', '_siguiente'
 
@@ -32,18 +33,10 @@ class ListaEnlazada:
     def mostrar(self):
         p = self._frente
         while p:
-            print(p._elemento,end=' --> ')#end, argumento que permite agregar texto al final de una cadena
+            print(p._elemento,end=' --> ')
+            #time.sleep(0.25)#end, argumento que permite agregar texto al final de una cadena
             p = p._siguiente
 
-    def buscar(self,key):
-        p = self._frente
-        index = 0
-        while p:
-            if p._elemento == key:
-                return index
-            p = p._siguiente
-            index += 1
-        return -1
 
     def añadir_inicio(self,e):
         nuevo = _Nodo(e, None)
@@ -55,17 +48,6 @@ class ListaEnlazada:
             self._frente = nuevo
         self._size += 1
 
-    def añadir_posicion(self,e,posicion):
-        nuevo = _Nodo(e,None)
-        p = self._frente
-        i = 1
-        while i < posicion:
-            p = p._siguiente
-            i = i + 1
-        nuevo._siguiente = p._siguiente
-        p._siguiente = nuevo
-        self._size += 1
-
     def eliminar_inicio(self):
         i = self._frente._elemento
         self._frente=self._frente._siguiente
@@ -73,6 +55,7 @@ class ListaEnlazada:
         return i
 
     def eliminar_final(self):
+        global i
         p = self._frente
         while p:
             if p._siguiente == self._final:
@@ -80,18 +63,7 @@ class ListaEnlazada:
                 p._siguiente = None
             p = p._siguiente
         self._size -= 1
-        return i
-
-    def eliminar_posicion(self,posicion):
-        p = self._frente
-        i = 1
-        while i < posicion:
-            p = p._siguiente
-            i = i + 1
-        e = p._siguiente._elemento
-        p._siguiente = p._siguiente._siguiente
-        self._size -= 1
-        return e
+        print(i)
 
 def movimiento(T1,T2,m):
     T1.añadir_final(T2._final._elemento)
@@ -114,37 +86,40 @@ def mostrar_todo():
     print("\nLista 2")
     L2.mostrar()
 
+def eliminacion_alterna(T1,T2,m):
+    if T1._size == 1:
+        print("\nSe ha eliminado el dato ",T1.eliminar_inicio())
+    else:
+        print("\nSe ha eliminado el dato ",T1.eliminar_final())
+    if T2._size == 1:
+        print("\nSe ha eliminado el dato ",T2.eliminar_inicio())
+    else:
+        print("\nSe ha eliminado el dato ",T2.eliminar_final())
+    print("\n-------------------------------------------------------------------------------------")
+    print("\nMovimiento de eliminacion ", m)
+    mostrar_todo()
+
+
+
 
 L1 = ListaEnlazada()
 L2 = ListaEnlazada()
 
 tamano = int(input("\nPor favor ingrese el número de datos que desea ingresar: "))
 
-i = 0
-while i < tamano:
-    L1.añadir_final(int(input(f"\nPor favor ingrese el numero en la posición {i}\t")))
+j = 0
+while j < tamano:
+    L1.añadir_final(int(input(f"\nPor favor ingrese el numero en la posición {j}\t")))
     L1.mostrar()
-    i += 1
+    j += 1
 
 
 movimiento(L2,L1,1)
 movimiento(L2,L1,2)
 movimiento(L2,L1,3)
 
-i = 0
-while i < 6:
-    print("\n\nElemento eliminado\t",L1.eliminar_final())
-    time.sleep(0.5)
-    L1.mostrar()
-    time.sleep(0.5)
-    i += 1
-    if L1._size == 0:
-        print("La lista esta vacia\n")
+eliminacion_alterna(L1,L2,1)
+eliminacion_alterna(L1,L2,2)
+eliminacion_alterna(L1,L2,3)
 
-    print("\n\nElemento eliminado\t",L2.eliminar_final())
-    time.sleep(0.5)
-    L2.mostrar()
-    time.sleep(0.5)
-    i += 1
-    if L2._size == 0:
-        print("La lista esta vacia\n")
+
